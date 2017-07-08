2774c8f3e5967b2ecceede18c9116b9640b9b500
Akila Hennayake <Akila Hennayake>
2017-07-08 15:02:25 +0400

    travel agent2 added to test routes working

---
+++ resources/views/backend/travelagent2.blade.php
@@ -0,0 +1,378 @@
+@extends('mainta')
+@section('admintacontent')
+
+    <div class="panel-body">
+        <div class="card">
+            <div class="col-lg-12">
+                <table width="100%" class="table table-striped table-bordered table-hover dataTable no-footer dtr-inline" id="dataTables-example" role="grid" aria-describedby="dataTables-example_info" style="width: 100%;">
+                    <thead>
+                    <tr role="row">
+                        <th class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Rendering engine: activate to sort column descending" style="width: 206px;" aria-sort="ascending">Date of Booking</th>
+                        <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 169px;">Reservation#</th>
+                        <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 187px;">Total People</th>
+                        <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 147px;">Total Price</th>
+                        <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 107px;">TA Profit</th>
+                        <th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 107px;">Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">12/4/2017</td>
+                        <td>21543001</td>
+                        <td class="">5</td>
+                        <td class="center">14,300</td>
+                        <td class="center">1,300</td>
+                        <td class="center">Pending</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    <tr class="gradeA odd" role="row">
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                        <td class="sorting_1">&nbsp;</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- /.panel-body -->
+
+    <!-- /.col-lg-8 -->
+
+    {{------------------------DO NOT REMOVE THE CLOSING DIVS BELOW-------------------------}}
+    </div>
+    <!-- /.will go content here -->
+
+
+    </div>
+    {{--<div class="text-center p-b-20">  <button type="button" class="btn btn-danger m-t-20">BOOK NOW</button>--}}
+    {{--</div>--}}
+
+    {{--<div class="omra-package hand" data-toggle="collapse" data-target="#amt2" data-parent="#myGroup">--}}
+    {{--<header class="omra-package__header">--}}
+    {{--<h3>Standard Omra for 4 People</h3>--}}
+    {{--<div class="omra-package__attr"> <span>11 Days/10 Nights</span> <span class="pc">2800 Dhs</span> </div>--}}
+    {{--</header>--}}
+    {{--<div class="row omra-package__tour">--}}
+    {{--<div class="col-xs-6">--}}
+    {{--<div class="pull-left m-r-10">--}}
+    {{--<h3>Madina</h3>--}}
+    {{--<small>8 Nights</small> </div>--}}
+    {{--<div class=" text-center  nopadding pull-left"> <img src="img/hlogo1.png" alt=""> </div>--}}
+
+    {{--</div>--}}
+
+
+    {{--<div class="col-xs-6">--}}
+    {{--<div class="pull-right m-l-10">--}}
+    {{--<h3>Madina</h3>--}}
+    {{--<small>8 Nights</small> </div>--}}
+    {{--<div class=" text-center nopadding pull-right "> <img src="img/hlogo2.png" alt=""> </div>--}}
+
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="omra-package__progress"> <span><i style="right: 20%;"></i></span> </div>--}}
+    {{--<div class="p-10"></div>--}}
+    {{--<div id="amt2" class="collapse ">--}}
+    {{--<div class="tbl">--}}
+    {{--<table class="table table-bordered text-center">--}}
+    {{--<thead>--}}
+    {{--<tr>--}}
+    {{--<th>Hotel Name</th>--}}
+    {{--<th>Keys</th>--}}
+    {{--<th>Hotel Name</th>--}}
+    {{--</tr>--}}
+    {{--</thead>--}}
+    {{--<tbody>--}}
+    {{--<tr>--}}
+    {{--<td>2</td>--}}
+    {{--<td><b>Bedroom</b></td>--}}
+    {{--<td>3</td>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+    {{--<td>Yes</td>--}}
+    {{--<td><b>parking</b></td>--}}
+    {{--<td>No</td>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+    {{--<td>No</td>--}}
+    {{--<td><b>Garden</b></td>--}}
+    {{--<td>Yes </td>--}}
+    {{--</tr>--}}
+    {{--</tbody>--}}
+    {{--</table>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="form-group  m-b-0">--}}
+    {{--<div class="pl ">  <i class="fa fa-caret-down fa-2x mdc-text-red m-t-5" aria-hidden="true "> </i> </div> </div>--}}
+
+
+    {{--</div>--}}
+    {{--<div class="text-center p-b-20">  <button type="button" class="btn btn-danger m-t-20">BOOK NOW</button>--}}
+    {{--</div>--}}
+
+    {{--<div class="omra-package hand" data-toggle="collapse" data-target="#amt3" data-parent="#myGroup">--}}
+    {{--<header class="omra-package__header">--}}
+    {{--<h3>Standard Omra for 4 People</h3>--}}
+    {{--<div class="omra-package__attr"> <span>11 Days/10 Nights</span> <span class="pc">2800 Dhs</span> </div>--}}
+    {{--</header>--}}
+    {{--<div class="row omra-package__tour">--}}
+    {{--<div class="col-xs-6">--}}
+    {{--<div class="pull-left m-r-10">--}}
+    {{--<h3>Madina</h3>--}}
+    {{--<small>8 Nights</small> </div>--}}
+    {{--<div class=" text-center  nopadding pull-left"> <img src="img/hlogo1.png" alt=""> </div>--}}
+
+    {{--</div>--}}
+
+
+    {{--<div class="col-xs-6">--}}
+    {{--<div class="pull-right m-l-10">--}}
+    {{--<h3>Madina</h3>--}}
+    {{--<small>8 Nights</small> </div>--}}
+    {{--<div class=" text-center nopadding pull-right "> <img src="img/hlogo2.png" alt=""> </div>--}}
+
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="omra-package__progress"> <span><i style="right: 20%;"></i></span> </div>--}}
+    {{--<div class="p-10"></div>--}}
+    {{--<div id="amt3" class="collapse ">--}}
+    {{--<div class="tbl">--}}
+    {{--<table class="table table-bordered text-center">--}}
+    {{--<thead>--}}
+    {{--<tr>--}}
+    {{--<th>Hotel Name</th>--}}
+    {{--<th>Keys</th>--}}
+    {{--<th>Hotel Name</th>--}}
+    {{--</tr>--}}
+    {{--</thead>--}}
+    {{--<tbody>--}}
+    {{--<tr>--}}
+    {{--<td>2</td>--}}
+    {{--<td><b>Bedroom</b></td>--}}
+    {{--<td>3</td>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+    {{--<td>Yes</td>--}}
+    {{--<td><b>parking</b></td>--}}
+    {{--<td>No</td>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+    {{--<td>No</td>--}}
+    {{--<td><b>Garden</b></td>--}}
+    {{--<td>Yes </td>--}}
+    {{--</tr>--}}
+    {{--</tbody>--}}
+    {{--</table>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="form-group  m-b-0">--}}
+    {{--<div class="pl ">  <i class="fa fa-caret-down fa-2x mdc-text-red m-t-5" aria-hidden="true "> </i> </div> </div>--}}
+
+
+    {{--</div>--}}
+    {{--<div class="text-center p-b-20">  <button type="button" class="btn btn-danger m-t-20">BOOK NOW</button>--}}
+    {{--</div>--}}
+
+
+    {{--<div class="omra-package hand" data-toggle="collapse" data-target="#amt4" data-parent="#myGroup">--}}
+    {{--<header class="omra-package__header">--}}
+    {{--<h3>Standard Omra for 4 People</h3>--}}
+    {{--<div class="omra-package__attr"> <span>11 Days/10 Nights</span> <span class="pc">2800 Dhs</span> </div>--}}
+    {{--</header>--}}
+    {{--<div class="row omra-package__tour">--}}
+    {{--<div class="col-xs-6">--}}
+    {{--<div class="pull-left m-r-10">--}}
+    {{--<h3>Madina</h3>--}}
+    {{--<small>8 Nights</small> </div>--}}
+    {{--<div class=" text-center  nopadding pull-left"> <img src="img/hlogo1.png" alt=""> </div>--}}
+
+    {{--</div>--}}
+
+
+    {{--<div class="col-xs-6">--}}
+    {{--<div class="pull-right m-l-10">--}}
+    {{--<h3>Madina</h3>--}}
+    {{--<small>8 Nights</small> </div>--}}
+    {{--<div class=" text-center nopadding pull-right "> <img src="img/hlogo2.png" alt=""> </div>--}}
+
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="omra-package__progress"> <span><i style="right: 20%;"></i></span> </div>--}}
+    {{--<div class="p-10"></div>--}}
+    {{--<div id="amt4" class="collapse ">--}}
+    {{--<div class="tbl">--}}
+    {{--<table class="table table-bordered text-center">--}}
+    {{--<thead>--}}
+    {{--<tr>--}}
+    {{--<th>Hotel Name</th>--}}
+    {{--<th>Keys</th>--}}
+    {{--<th>Hotel Name</th>--}}
+    {{--</tr>--}}
+    {{--</thead>--}}
+    {{--<tbody>--}}
+    {{--<tr>--}}
+    {{--<td>2</td>--}}
+    {{--<td><b>Bedroom</b></td>--}}
+    {{--<td>3</td>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+    {{--<td>Yes</td>--}}
+    {{--<td><b>parking</b></td>--}}
+    {{--<td>No</td>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+    {{--<td>No</td>--}}
+    {{--<td><b>Garden</b></td>--}}
+    {{--<td>Yes </td>--}}
+    {{--</tr>--}}
+    {{--</tbody>--}}
+    {{--</table>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="form-group  m-b-0">--}}
+    {{--<div class="pl ">  <i class="fa fa-caret-down fa-2x mdc-text-red m-t-5" aria-hidden="true "> </i> </div> </div>--}}
+
+
+    {{--</div>--}}
+    {{--<div class="text-center p-b-20">  <button type="button" class="btn btn-danger m-t-20">BOOK NOW</button>--}}
+    {{--</div>--}}
+@endsection
